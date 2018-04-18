93b3ce82f886264abc01875b677203ad8426e925
Jeramz <36828615+Jeramz@users.noreply.github.com>
2018-04-17 20:33:51 -0500

    Update BinaryFile.java

--- src/GestionDeMatriculasBinaryFile/BinaryFile.java
+++ src/GestionDeMatriculasBinaryFile/BinaryFile.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ Clase GestionMatriculasGUI: Menú para el usuario
+  Autor: Jesus Ramirez-1731388  Samuel Velasco-1731295 Andrés Felipe-1730534
+  email: jesus.zuluaga@correounivalle.edu.co - samuel.velasco@correounivalle.edu.co - andres.lopez@correounivalle.edu.co
+  fecha: 21 Marzo 2018
  */
 package GestionDeMatriculasBinaryFile;
 
